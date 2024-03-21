640c014c03a36ef51b700929d24ddcba2c2d71d4
imincik <imincik@users.noreply.github.com>
2024-03-21 14:30:11 +0000

    pkgs: automatic update of packages database

--- src/GeoPackages.elm
+++ src/GeoPackages.elm
@@ -11,11 +11,11 @@ packages =
     , ( "geopkgs.libspatialindex", "1.9.3" )
     , ( "geopkgs.libspatialite", "5.1.0" )
     , ( "geopkgs.nixGL", "" )
-    , ( "geopkgs.pdal", "2.6.3" )
+    , ( "geopkgs.pdal", "2.7.0" )
     , ( "geopkgs.pg_featureserv", "1.3.1" )
     , ( "geopkgs.pg_tileserv", "1.0.11" )
     , ( "geopkgs.proj", "9.4.0" )
     , ( "geopkgs.qgis", "3.36.0" )
-    , ( "geopkgs.qgis-ltr", "3.28.15" )
+    , ( "geopkgs.qgis-ltr", "3.34.4" )
     , ( "geopkgs.tiledb", "2.18.2" )
     ]
