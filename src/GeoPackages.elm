248cc58f165d9705feb70e060e431a1a6764ca9a
imincik <476346+imincik@users.noreply.github.com>
2024-07-22 01:32:06 +0000

    pkgs: automatic update of packages database

--- src/GeoPackages.elm
+++ src/GeoPackages.elm
@@ -3,7 +3,7 @@ module GeoPackages exposing (packages)
 
 packages =
     [ ( "geopkgs.gdal", "3.9.1" )
-    , ( "geopkgs.gdal-master", "1b32f9f" )
+    , ( "geopkgs.gdal-master", "" )
     , ( "geopkgs.gdal-minimal", "3.9.1" )
     , ( "geopkgs.geos", "3.12.2" )
     , ( "geopkgs.grass", "8.3.2" )
@@ -11,7 +11,7 @@ packages =
     , ( "geopkgs.librttopo", "1.1.0" )
     , ( "geopkgs.libspatialindex", "1.9.3" )
     , ( "geopkgs.libspatialite", "5.1.0" )
-    , ( "geopkgs.pdal", "2.7.1" )
+    , ( "geopkgs.pdal", "2.7.2" )
     , ( "geopkgs.pg_featureserv", "1.3.1" )
     , ( "geopkgs.pg_tileserv", "1.0.11" )
     , ( "geopkgs.proj", "9.4.1" )
