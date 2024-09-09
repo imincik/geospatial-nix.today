d312bef93278eb51a72d47c3079d1eb72d8ff3df
imincik <476346+imincik@users.noreply.github.com>
2024-09-09 01:33:15 +0000

    pkgs: automatic update of packages database

--- src/GeoPythonPackages.elm
+++ src/GeoPythonPackages.elm
@@ -2,7 +2,7 @@ module GeoPythonPackages exposing (packages)
 
 
 packages =
-    [ ( "geopkgs.python3-fiona", "1.9.6" )
+    [ ( "geopkgs.python3-fiona", "1.10.0" )
     , ( "geopkgs.python3-gdal", "3.9.2" )
     , ( "geopkgs.python3-geopandas", "1.0.1" )
     , ( "geopkgs.python3-owslib", "0.31.0" )
@@ -11,6 +11,6 @@ packages =
     , ( "geopkgs.python3-pyproj", "3.6.1" )
     , ( "geopkgs.python3-pyqt5", "5.15.9" )
     , ( "geopkgs.python3-pystac", "1.10.1" )
-    , ( "geopkgs.python3-rasterio", "1.3.10" )
+    , ( "geopkgs.python3-rasterio", "1.3.11" )
     , ( "geopkgs.python3-shapely", "2.0.6" )
     ]
