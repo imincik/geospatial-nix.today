1e18da6b40069c535423c0ed0db640ca74c6cd4e
imincik <476346+imincik@users.noreply.github.com>
2024-10-07 01:32:52 +0000

    pkgs: automatic update of packages database

--- src/GeoPackages.elm
+++ src/GeoPackages.elm
@@ -3,9 +3,9 @@ module GeoPackages exposing (packages)
 
 packages =
     [ ( "geopkgs.gdal", "3.9.2" )
-    , ( "geopkgs.gdal-master", "8055974" )
+    , ( "geopkgs.gdal-master", "3a9bf85" )
     , ( "geopkgs.gdal-minimal", "3.9.2" )
-    , ( "geopkgs.geos", "3.12.2" )
+    , ( "geopkgs.geos", "3.13.0" )
     , ( "geopkgs.libgeotiff", "1.7.3" )
     , ( "geopkgs.librttopo", "1.1.0" )
     , ( "geopkgs.libspatialindex", "2.0.0" )
--- src/GeoPythonPackages.elm
+++ src/GeoPythonPackages.elm
@@ -7,10 +7,10 @@ packages =
     , ( "geopkgs.python3-geopandas", "1.0.1" )
     , ( "geopkgs.python3-owslib", "0.31.0" )
     , ( "geopkgs.python3-psycopg", "3.1.17" )
-    , ( "geopkgs.python3-pyogrio", "0.9.0" )
+    , ( "geopkgs.python3-pyogrio", "0.10.0" )
     , ( "geopkgs.python3-pyproj", "3.6.1" )
     , ( "geopkgs.python3-pyqt5", "5.15.9" )
-    , ( "geopkgs.python3-pystac", "1.10.1" )
+    , ( "geopkgs.python3-pystac", "1.11.0" )
     , ( "geopkgs.python3-rasterio", "1.3.11" )
     , ( "geopkgs.python3-shapely", "2.0.6" )
     ]
