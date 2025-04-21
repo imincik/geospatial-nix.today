1817d00174246e106fc191b88a5ede2649213d83
imincik <476346+imincik@users.noreply.github.com>
2025-04-21 01:34:20 +0000

    pkgs: automatic update of packages database

--- src/QGISPackages.elm
+++ src/QGISPackages.elm
@@ -2,6 +2,6 @@ module QGISPackages exposing (packages)
 
 
 packages =
-    [ ( "pkgs.qgis", "3.42.0" )
-    , ( "pkgs.qgis-ltr", "3.40.4" )
+    [ ( "pkgs.qgis", "3.42.1" )
+    , ( "pkgs.qgis-ltr", "3.40.5" )
     ]
